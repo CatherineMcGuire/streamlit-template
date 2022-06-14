ab46a3b658f455d80716df721b015133a81b81e0
Catherine McGuire <catherine.mcguire@manchester.ac.uk>
2022-06-14 18:44:41 +0100

    Updates model

--- home.py
+++ home.py
@@ -1,10 +1,31 @@
 # A simple model/view template for Streamlit
 
 import streamlit as st
+import pandas as pd
 
 
 class Model:
-    caption = "This is some text"
+    class Model:
+        def __init__(self):
+            self.df = pd.DataFrame(px.data.gapminder())
+            self.ylist = [int(i) for i in self.df['year'].unique()]
+            self.yearStart = self.ylist[0]
+            self.yearEnd = self.ylist[-1]
+            self.yearStep = self.ylist[1] - self.ylist[0]
+
+        def chart(self, year):
+            return px.scatter(self.df[self.df['year'] == year],
+                              x='lifeExp', y='gdpPercap',
+                              title=f'Year: {year}',
+                              color='continent', size='pop')
+
+        header = 'Global Statistics from Gapminder'
+        description = '''
+          See how life expectancy changes over time 
+          and in relation to GDP.
+          Move the slider to change the year to display.
+       '''
+        sliderCaption = 'Select the year for the chart'
 
 # View
 
