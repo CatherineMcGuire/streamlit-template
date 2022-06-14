98f019f1322c96e0c973941a294b6572ee8d1291
Catherine McGuire <catherine.mcguire@manchester.ac.uk>
2022-06-14 16:52:13 +0100

    Updates view

--- home.py
+++ home.py
@@ -1,6 +1,36 @@
+# A simple model/view template for Streamlit
+
 import streamlit as st
 
-st.write("This is the home page")
+
+class Model:
+    caption = "This is some text"
+
+# View
+
+
+def view(model):
+    st.set_page_config(layout='wide')
+    # Header
+    st.header(model.header)
+
+    commentaryCol, spaceCol, chartCol = st.columns((2, 1, 6))
+    # Description
+    with commentaryCol:
+        st.write(model.description)
+    # Year Slider
+    year = st.slider(model.sliderCaption,
+                     model.yearStart, model.yearEnd,
+                     model.yearStart, model.yearStep)
+    # Chart
+    with chartCol:
+        st.plotly_chart(model.chart(year),
+                        use_container_width=True)
+
+# Start
+
+
+view(Model())
 
 
 
