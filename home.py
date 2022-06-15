944c024df2f9c9ed081fbded7133678391e4aa71
Catherine McGuire <catherine.mcguire@manchester.ac.uk>
2022-06-15 14:52:56 +0100

    Fixes typos

--- home.py
+++ home.py
@@ -2,30 +2,31 @@
 
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 
 class Model:
-    class Model:
-        def __init__(self):
-            self.df = pd.DataFrame(px.data.gapminder())
-            self.ylist = [int(i) for i in self.df['year'].unique()]
-            self.yearStart = self.ylist[0]
-            self.yearEnd = self.ylist[-1]
-            self.yearStep = self.ylist[1] - self.ylist[0]
+    def __init__(self):
+        self.df = pd.DataFrame(px.data.gapminder())
+        self.ylist = [int(i) for i in self.df['year'].unique()]
+        self.yearStart = self.ylist[0]
+        self.yearEnd = self.ylist[-1]
+        self.yearStep = self.ylist[1] - self.ylist[0]
 
-        def chart(self, year):
-            return px.scatter(self.df[self.df['year'] == year],
-                              x='lifeExp', y='gdpPercap',
-                              title=f'Year: {year}',
-                              color='continent', size='pop')
+    def chart(self, year):
+        return px.scatter(self.df[self.df['year'] == year],
+                          x='lifeExp', y='gdpPercap',
+                          title=f'Year: {year}',
+                          color='continent', size='pop')
 
-        header = 'Global Statistics from Gapminder'
-        description = '''
-          See how life expectancy changes over time 
-          and in relation to GDP.
-          Move the slider to change the year to display.
-       '''
-        sliderCaption = 'Select the year for the chart'
+    header = 'Global Statistics from Gapminder'
+
+    description = '''
+      See how life expectancy changes over time 
+      and in relation to GDP.
+      Move the slider to change the year to display.
+   '''
+    sliderCaption = 'Select the year for the chart'
 
 # View
 
@@ -35,16 +36,17 @@ def view(model):
     # Header
     st.header(model.header)
 
-    commentaryCol, spaceCol, chartCol = st.columns((2, 1, 6))
+    commentary_col, space_col, chart_col = st.columns((2, 1, 6))
     # Description
-    with commentaryCol:
+    with commentary_col:
         st.write(model.description)
+
     # Year Slider
-    year = st.slider(model.sliderCaption,
-                     model.yearStart, model.yearEnd,
-                     model.yearStart, model.yearStep)
+        year = st.slider(model.sliderCaption,
+                         model.yearStart, model.yearEnd,
+                         model.yearStart, model.yearStep)
     # Chart
-    with chartCol:
+    with chart_col:
         st.plotly_chart(model.chart(year),
                         use_container_width=True)
 
